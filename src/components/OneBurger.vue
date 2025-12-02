b283401cea48e060262d7bd9a37157ccdd6f00e2
Tobias F. Lindkvist <tobias.franksson.lindkvist@gmail.com>
2025-12-02 22:47:56 +0100

    added checklist for lab IV-V and fixed formatting

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,43 +1,45 @@
 <template>
-            <div class="menu-item">
-              <h3>{{burger.name}}</h3>
-              <img
-                v-bind:src="burger.imgURL"
-              />
-              <p>
-                <button v-on:click="updateAmount(-1)">-</button>
-                <span>{{ amountOrdered }}</span>
-                <button v-on:click="updateAmount(1)">+</button>
-              </p>
-              <ul>
-                <li>{{ burger.kcal }} kCal</li>
-                <li v-if="burger.hasGluten">Contains <span class="allergies">gluten</span></li>
-                <li v-if="burger.hasLactose">Contains <span class="allergies">lactose</span></li>
-              </ul>
-              </div>
+  <div class="menu-item">
+    <h3>{{ burger.name }}</h3>
+    <img v-bind:src="burger.imgURL" />
+    <p>
+      <button v-on:click="updateAmount(-1)">-</button>
+      <span>{{ amountOrdered }}</span>
+      <button v-on:click="updateAmount(1)">+</button>
+    </p>
+    <ul>
+      <li>{{ burger.kcal }} kCal</li>
+      <li v-if="burger.hasGluten">
+        Contains <span class="allergies">gluten</span>
+      </li>
+      <li v-if="burger.hasLactose">
+        Contains <span class="allergies">lactose</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'OneBurger',
+  name: "OneBurger",
   props: {
-    burger: Object
+    burger: Object,
   },
-  data: function() {
+  data: function () {
     return {
       amountOrdered: 0,
-    }
+    };
   },
   methods: {
-    updateAmount: function(amount) {
+    updateAmount: function (amount) {
       if (this.amountOrdered + amount < 0) {
-        amount = -this.amountOrdered
+        amount = -this.amountOrdered;
       }
       this.amountOrdered += amount;
-      this.$emit('amountUpdated', {amount: this.amountOrdered})
-    }
-  }
-}
+      this.$emit("amountUpdated", { amount: this.amountOrdered });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,93 +1,105 @@
 <template>
-
-<header>
-      <div class="header">
-        <h1>Welcome to Mc10 Online</h1>
-      </div>
-    </header>
-    <main>
-      <section class="menu">
-        <h2>Select Item</h2>
-        <p>Please select which burger your tummy would find yummy.</p>
-        <div class="menu-list">
-        <Burger v-for="burger in burgers"
+  <header>
+    <div class="header">
+      <h1>Welcome to Mc10 Online</h1>
+    </div>
+  </header>
+  <main>
+    <section class="menu">
+      <h2>Select Item</h2>
+      <p>Please select which burger your tummy would find yummy.</p>
+      <div class="menu-list">
+        <Burger
+          v-for="burger in burgers"
           v-bind:burger="burger"
           v-bind:key="burger.name"
-          v-on:amountUpdated="addToOrder(burger.name, $event.amount)"/>
-        </div>
-      </section>
-      <section class="order-info">
-        <h2>Customer Information</h2>
-        <p>
-          Please enter delivery information below so we can find you. And we
-          will feed you.
-        </p>
-        <h3>Delivery Information</h3>
-        <p>
-          <label for="fullname">Full Name</label>
-          <input required v-model="order.name" placeholder="First and last name" />
-        </p>
-        <p>
-          <label for="email">Email Address</label>
-          <input
-            required
-            type="email"
-            v-model="order.email"
-            id="email"
-            placeholder="e-mail"
-          />
-        </p>
-        <p>
-          <label for="payment">Payment Option</label>
-          <select v-model="order.paymentoption" id="payment">
-            <option value="Credit Card">Credit Card</option>
-            <option value="Swish">Swish</option>
-            <option value="Klarna">Klarna</option>
-            <option value="Cash">Cash in envelope</option>
-          </select>
-        </p>
-        <fieldset>
-          <legend>Select Gender</legend>
+          v-on:amountUpdated="addToOrder(burger.name, $event.amount)"
+        />
+      </div>
+    </section>
+    <section class="order-info">
+      <h2>Customer Information</h2>
+      <p>
+        Please enter delivery information below so we can find you. And we will
+        feed you.
+      </p>
+      <h3>Delivery Information</h3>
+      <p>
+        <label for="fullname">Full Name</label>
+        <input
+          required
+          v-model="order.name"
+          placeholder="First and last name"
+        />
+      </p>
+      <p>
+        <label for="email">Email Address</label>
+        <input
+          required
+          type="email"
+          v-model="order.email"
+          id="email"
+          placeholder="e-mail"
+        />
+      </p>
+      <p>
+        <label for="payment">Payment Option</label>
+        <select v-model="order.paymentoption" id="payment">
+          <option value="Credit Card">Credit Card</option>
+          <option value="Swish">Swish</option>
+          <option value="Klarna">Klarna</option>
+          <option value="Cash">Cash in envelope</option>
+        </select>
+      </p>
+      <fieldset>
+        <legend>Select Gender</legend>
 
-          <input type="radio" v-model="order.gender" value="Male" id="male" />
-          <label for="male">Male</label>
+        <input type="radio" v-model="order.gender" value="Male" id="male" />
+        <label for="male">Male</label>
 
-          <input type="radio" v-model="order.gender" value="Female" id="female" />
-          <label for="female">Female</label>
+        <input type="radio" v-model="order.gender" value="Female" id="female" />
+        <label for="female">Female</label>
 
-          <input type="radio" v-model="order.gender" value="Non-binary" id="nonbinary" />
-          <label for="nonbinary">Non-binary</label>
+        <input
+          type="radio"
+          v-model="order.gender"
+          value="Non-binary"
+          id="nonbinary"
+        />
+        <label for="nonbinary">Non-binary</label>
 
-          <input type="radio" v-model="order.gender" value="None" id="none" />
-          <label for="none">Do not wish to provide</label>
-        </fieldset>
-      </section>
+        <input type="radio" v-model="order.gender" value="None" id="none" />
+        <label for="none">Do not wish to provide</label>
+      </fieldset>
+    </section>
 
     <div class="map-container">
       <div id="map" v-on:click="setLocation">
-        <div class="dot" v-bind:style="{ left: location.x + 'px', top: location.y + 'px'}" >
+        <div
+          class="dot"
+          v-bind:style="{ left: location.x + 'px', top: location.y + 'px' }"
+        >
           T
         </div>
       </div>
     </div>
-      <p>
-        <button v-on:click="addOrder()">
-          <img src="/img/fragbag.png" alt="" />
-          <span>Place order</span>
-        </button>
-      </p>
-    </main>
-    <hr />
-    <footer>
-      <span>&copy; 2025 Mc10 Burger and Beans llc. </span>
-    </footer>
-
+    <p>
+      <button v-on:click="addOrder()">
+        <img src="/img/fragbag.png" alt="" />
+        <span>Place order</span>
+      </button>
+    </p>
+  </main>
+  <hr />
+  <footer>
+    <span>&copy; 2025 Mc10 Burger and Beans llc. </span>
+  </footer>
 </template>
 
 <script>
-import Burger from '../components/OneBurger.vue'
-import io from 'socket.io-client'
-import menu from '../assets/menu.json'
+import Burger from "../components/OneBurger.vue";
+import io from "socket.io-client";
+import menu from "../assets/menu.json";
 
 const socket = io("localhost:3000");
 
@@ -99,9 +111,9 @@ function MenuItem(name, imgURL, kcal, hasGluten, hasLactose) {
   this.hasLactose = hasLactose;
 }
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Burger
+    Burger,
   },
   data: function () {
     return {
@@ -114,43 +126,47 @@ export default {
       },
       orderedBurgers: {},
       location: { x: 0, y: 0 },
-    }
+    };
   },
   methods: {
     getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+      return Math.floor(Math.random() * 100000);
     },
     addOrder: function () {
-      socket.emit("addOrder", { orderId: this.getOrderNumber(),
+      socket.emit("addOrder", {
+        orderId: this.getOrderNumber(),
         details: {
           location: this.location,
           name: this.order.name,
           email: this.order.email,
           payment: this.order.paymentoption,
-          gender: this.order.gender
+          gender: this.order.gender,
         },
-        orderItems: this.orderedBurgers});
+        orderItems: this.orderedBurgers,
+      });
     },
-    setLocation: function(event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
+    setLocation: function (event) {
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
       this.location = {
         x: event.clientX - 10 - offset.x,
-        y: event.clientY - 10 - offset.y
-      }
+        y: event.clientY - 10 - offset.y,
+      };
     },
-    addToOrder: function(itemName, amount) {
+    addToOrder: function (itemName, amount) {
       this.orderedBurgers[itemName] = amount;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
 #map {
-    width: 1920px;
-    height: 1078px;
-    background-image: url("/img/polacks.jpg");
+  width: 1920px;
+  height: 1078px;
+  background-image: url("/img/polacks.jpg");
 }
 
 .dot {
@@ -158,8 +174,8 @@ export default {
   background: black;
   color: white;
   border-radius: 10px;
-  width:20px;
-  height:20px;
+  width: 20px;
+  height: 20px;
   text-align: center;
 }
 
